15a28eb33405570267fd72d895852fc42ab62e71
Kieran Paranjpe <kieranbellum@gmail.com>
2024-07-25 14:46:48 -0700

    Update BreathInputHandler.cs

--- Assets/Scripts/BreathInputHandler.cs
+++ Assets/Scripts/BreathInputHandler.cs
@@ -1,36 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-/* Goals of script:
- * Handle all aspects of Input. Will manage all different types of inputs and their scripts.
- *
- * Usage:
- * 1. Put 'BreathInputHandler' on any gameobject, and then add either/both PitchEstimator/BreathInference to the object depending on inputs you want to capture
- * 2. Setup all fields including microphoneDeviceIndex and sampleRate, as well as settings on individual scripts
- * 3. For BreathInference, ensure that the settings reflect the neural network model, and assign your chosen NN model from the assets folder.
- * Key settings for each model can be found below:
- * test_0_4096:
- * SAMPLE RATE: 16000, SAMPLES PER CHUNK: 4096, NFFT: 4096
- * test_0_8000:
- * SAMPLE RATE: 16000, SAMPLES PER CHUNK: 8000, NFFT: 2048
- * 4. For PitchEstimator, tinker with tolerance percent (0<t<1). This determines on a relative scale how close you must be to the note in order for it to register.
- *
- * Get Input from other scripts:
- * Use BreathInference.BreathClassification and BreathInference.BreathClassificationCertainty
- * and
- * Use PitchEstimator.CurrentNote and PitchEstimator.CurrentFrequency
- *
- * To change the microphone device, use:
- * MicrophoneListener.Instance.BeginMicrophoneStream(microphoneDeviceIndex, clipLength, sampleRate); -> if there is already a stream, it will replace the old one
- * You can also change the AudioSource with:
- * MicrophoneListener.Instance.AttachStreamToAudioSource(audioSource); -> if already an audio source, removes old one
- * and
- * MicrophoneListener.Instance.DetachStreamFromAudioSource();
- * 
- * 
- */
-
+/// <summary>
+/// Goals of script: <br></br>
+/// Handle all aspects of Input. Will manage all different types of inputs and their scripts.<br></br>
+/// Usage:<br></br>
+/// 1. Put 'BreathInputHandler' on any gameobject, and then add either/both PitchEstimator/BreathInference to the object depending on inputs you want to capture<br></br>
+/// 2. Setup all fields including microphoneDeviceIndex and sampleRate, as well as settings on individual scripts<br></br>
+/// 3. For BreathInference, ensure that the settings reflect the neural network model, and assign your chosen NN model from the assets folder.<br></br>
+/// Key settings for each model can be found below:<br></br>
+/// test_0_4096:<br></br>
+/// SAMPLE RATE: 16000, SAMPLES PER CHUNK: 4096, NFFT: 4096<br></br>
+/// test_0_8000:<br></br>
+/// SAMPLE RATE: 16000, SAMPLES PER CHUNK: 8000, NFFT: 2048<br></br>
+/// 4. For PitchEstimator, tinker with tolerance percent (0 to 1). This determines on a relative scale how close you must be to the note in order for it to register.<br></br>
+///<br></br>
+/// Get Input from other scripts:<br></br>
+/// Use BreathInference.BreathClassification and BreathInference.BreathClassificationCertainty<br></br>
+/// and<br></br>
+/// Use PitchEstimator.CurrentNote and PitchEstimator.CurrentFrequency<br></br>
+///<br></br>
+/// To change the microphone device, use:<br></br>
+/// MicrophoneListener.Instance.BeginMicrophoneStream(microphoneDeviceIndex, clipLength, sampleRate); -> if there is already a stream, it will replace the old one<br></br>
+/// You can also change the AudioSource with:<br></br>
+/// MicrophoneListener.Instance.AttachStreamToAudioSource(audioSource); -> if already an audio source, removes old one<br></br>
+/// and<br></br>
+/// MicrophoneListener.Instance.DetachStreamFromAudioSource();<br></br>
+/// <br></br>
+/// Import Scripts are: BreathInputHandler, MicrophoneListener, BreathInference, PitchEstimator and AudioTransformations<br></br>
+///  -> AudioTransformations relies on plugins in the Plugins folder.<br></br>
+/// </summary>
 public class BreathInputHandler : MonoBehaviour
 {
     [SerializeField] private BreathInference breathInference;
